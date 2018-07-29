497f6e40e9de92f747293ffcdf11e5348910bd9a
Astemir Eleev <astemireleev@gmail.com>
2018-07-29 18:51:28 +0300

    Implemented GameScene class

--- ios-spritekit-physics-racing Shared/GameScene.swift
+++ ios-spritekit-physics-racing Shared/GameScene.swift
@@ -10,6 +10,25 @@ import SpriteKit
 
 class GameScene: SKScene {
 
+    // MARK: - Properties
+    
+    var car: VehicleJeep!
+    
+    var hudNode: SKNode!
+    var cameraNode: SKCameraNode!
+    
+    var forwardNode: SKSpriteNode?
+    var backwardNode: SKSpriteNode?
+    var leftNode: SKSpriteNode?
+    var rightNode: SKSpriteNode?
+    
+    var isContinious = false
+    var isForward = false
+    var isBackward = false
+    var isLeft = false
+    var isRight = false
+    
+    // MARK: - Methods
     
     class func newGameScene() -> GameScene {
         // Load 'GameScene.sks' as an SKScene.
@@ -19,14 +38,44 @@ class GameScene: SKScene {
         }
         
         // Set the scale mode to scale to fit the window
-        scene.scaleMode = .aspectFill
+        scene.scaleMode = .resizeFill
         
         return scene
     }
     
     func setUpScene() {
         // Get label node from scene and store it for use later
+        hudNode = childNode(withName: "//HUD")
+        forwardNode = childNode(withName: "//Forward") as? SKSpriteNode
+        backwardNode = childNode(withName: "//Backward") as? SKSpriteNode
+        cameraNode = childNode(withName: "//SKCameraNode") as? SKCameraNode
+        leftNode = childNode(withName: "//Left") as? SKSpriteNode
+        rightNode = childNode(withName: "//Right") as? SKSpriteNode
         
+        #if os(macOS)
+        // Hide the on screen controlls when the target is macOS
+        hudNode.alpha = 0.0
+        #endif
+        
+        
+        self.camera = cameraNode
+        
+        let spawnPosition = childNode(withName: "//Spawn Position")
+        
+        car = VehicleJeep(position: spawnPosition?.position ?? .zero)
+        addChild(car)
+        
+        car.joints.forEach { joint in
+            self.physicsWorld.add(joint)
+        }
+        
+        
+        // CnConstraint the camera to follow the car
+        let zeroDistance = SKRange(constantValue: 0)
+        let playerConstraint = SKConstraint.distance(zeroDistance, to: car.chasis)
+        camera?.constraints = [playerConstraint]
+        camera?.xScale = 1.0
+        camera?.yScale = 1.0
         
     }
     
@@ -42,6 +91,22 @@ class GameScene: SKScene {
     
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
+        if isContinious {
+            if isForward {
+                car.applyForwardImpulse()
+            }
+            if isBackward {
+                car.applyBackwardImpulse()
+            }
+            
+            if isLeft {
+                car.applyLeftTilt()
+            }
+            
+            if isRight {
+                car.applyRightTilt()
+            }
+        }
     }
 }
 
@@ -50,20 +115,86 @@ class GameScene: SKScene {
 extension GameScene {
 
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-    
+        isContinious = true
         
-//        for t in touches {
-//            self.makeSpinny(at: t.location(in: self), color: SKColor.green)
-//        }
+        for touch in touches {
+            let location = touch.location(in: self)
+            let toucheNodes = nodes(at: location)
+            
+            for touchedNode in toucheNodes {
+                guard let node = touchedNode as? SKSpriteNode else {
+                    continue
+                }
+                
+                if node.name == "Forward" {
+                    isForward = true
+                }
+                if node.name == "Backward" {
+                    isBackward = true
+                }
+                if node.name == "Left" {
+                    isLeft = true
+                }
+                if node.name == "Right" {
+                    isRight = true
+                }
+            }
+        }
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            let location = touch.location(in: self)
+            let toucheNodes = nodes(at: location)
+            
+            for touchedNode in toucheNodes {
+                guard let node = touchedNode as? SKSpriteNode else {
+                    continue
+                }
+                
+                if node.name == "Forward" {
+                    isForward = false
+                }
+                if node.name == "Backward" {
+                    isBackward = false
+                }
+                if node.name == "Left" {
+                    isLeft = false
+                }
+                if node.name == "Right" {
+                    isRight = false
+                }
+            }
+        }
     }
     
     override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            let location = touch.location(in: self)
+            let toucheNodes = nodes(at: location)
+            
+            for touchedNode in toucheNodes {
+                guard let node = touchedNode as? SKSpriteNode else {
+                    continue
+                }
+                
+                if node.name == "Forward" {
+                    isForward = false
+                }
+                if node.name == "Backward"{
+                    isBackward = false
+                }
+                if node.name == "Left" {
+                    isLeft = false
+                }
+                if node.name == "Right" {
+                    isRight = false
+                }
+            }
+        }
     }
     
    
@@ -71,18 +202,74 @@ extension GameScene {
 #endif
 
 #if os(OSX)
+
+import Carbon
+
 // Mouse-based event handling
 extension GameScene {
 
+    // MARK: - Properties
+    
+    static let downArrow = UInt16(kVK_DownArrow)
+    static let leftArrow = UInt16(kVK_LeftArrow)
+    static let rightArrow = UInt16(kVK_RightArrow)
+    static let upArrow = UInt16(kVK_UpArrow)
+    
+    // MARK: - Mouse handling
+    
     override func mouseDown(with event: NSEvent) {
-        
-//        self.makeSpinny(at: event.location(in: self), color: SKColor.green)
     }
     
     override func mouseDragged(with event: NSEvent) {
-        
     }
+    
     override func mouseUp(with event: NSEvent) {
+    }
+    
+    // MARK: - Keyboard handling
+    
+    override func keyUp(with event: NSEvent) {
+        isContinious = false
+        
+        let keyCode = event.keyCode
+        
+        if keyCode == GameScene.leftArrow {
+            isBackward = false
+        }
+        
+        if keyCode == GameScene.rightArrow {
+            isForward = false
+        }
+        
+        if keyCode == GameScene.upArrow {
+            isLeft = false
+        }
+        
+        if keyCode == GameScene.downArrow {
+            isRight = false
+        }
+    }
+
+    override func keyDown(with event: NSEvent) {
+        isContinious = true
+        
+        let keyCode = event.keyCode
+        
+        if keyCode == GameScene.leftArrow {
+            isBackward = true
+        }
+        
+        if keyCode == GameScene.rightArrow {
+            isForward = true
+        }
+        
+        if keyCode == GameScene.upArrow {
+            isLeft = true
+        }
+        
+        if keyCode == GameScene.downArrow {
+            isRight = true
+        }
     }
 
 }
