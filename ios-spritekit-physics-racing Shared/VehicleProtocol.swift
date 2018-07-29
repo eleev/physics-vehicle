fceeb1e42d91470a298d52931e073ab69fa06425
Astemir Eleev <astemireleev@gmail.com>
2018-07-29 18:51:13 +0300

    Created Vehicle protocol

---
+++ ios-spritekit-physics-racing Shared/VehicleProtocol.swift
@@ -0,0 +1,14 @@
+//
+//  VehicleProtocol.swift
+//  ios-spritekit-physics-racing
+//
+//  Created by Astemir Eleev on 29/07/2018.
+//  Copyright © 2018 Astemir Eleev. All rights reserved.
+//
+
+import SpriteKit
+
+protocol VehicleProtocol: class {
+    var parts: [SKNode] { get set }
+    // In development
+}
