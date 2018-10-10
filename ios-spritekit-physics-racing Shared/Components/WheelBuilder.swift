c87660465f4f932c9235f0a46b92f3ddc5410a20
Astemir Eleev <astemireleev@gmail.com>
2018-10-10 19:25:58 +0300

    Crated WheelProtocol protocol

---
+++ ios-spritekit-physics-racing Shared/Components/WheelBuilder.swift
@@ -0,0 +1,17 @@
+//
+//  WheelBuilder.swift
+//  ios-spritekit-physics-racing iOS
+//
+//  Created by Astemir Eleev on 10/10/2018.
+//  Copyright © 2018 Astemir Eleev. All rights reserved.
+//
+
+import Foundation
+
+protocol WheelBuilder {
+    var texture: SKTexture { get }
+    var size: CGSize { get }
+    var zPostiion: CGFloat { get }
+    var position: CGPoint { get }
+    var allowsRotation: Bool { get }
+}
