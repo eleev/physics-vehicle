405f72c8f957238df4f3eb5150620f9a0341ba05
Astemir Eleev <astemireleev@gmail.com>
2018-10-10 19:26:18 +0300

    Created WheelVechileComponentsFactory struct

---
+++ ios-spritekit-physics-racing Shared/Components/WheelVehicleComponentsFactory.swift
@@ -0,0 +1,15 @@
+//
+//  WheelVehicleComponentsFactory.swift
+//  ios-spritekit-physics-racing iOS
+//
+//  Created by Astemir Eleev on 10/10/2018.
+//  Copyright © 2018 Astemir Eleev. All rights reserved.
+//
+
+import Foundation
+
+struct WheelVehicleComponentsFactory {
+    //    static func produceWheel() -> SKSpriteNode {
+    //
+    //    }
+}
