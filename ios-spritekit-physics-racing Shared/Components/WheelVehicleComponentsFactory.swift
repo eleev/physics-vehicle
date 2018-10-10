f6b041767fc006e92f47eac99e27f2ba18ac7d79
Astemir Eleev <astemireleev@gmail.com>
2018-10-10 19:46:39 +0300

    Implemented method signature for produce function

--- ios-spritekit-physics-racing Shared/Components/WheelVehicleComponentsFactory.swift
+++ ios-spritekit-physics-racing Shared/Components/WheelVehicleComponentsFactory.swift
@@ -6,10 +6,17 @@
 //  Copyright © 2018 Astemir Eleev. All rights reserved.
 //
 
-import Foundation
+import SpriteKit
 
 struct WheelVehicleComponentsFactory {
-    //    static func produceWheel() -> SKSpriteNode {
-    //
-    //    }
+    
+    /// Produces a vechicle wheel with physics-based suspention
+    ///
+    /// - Parameters:
+    ///     - builder: is a WheelBuilder that holds all the configuration for a wheel
+    /// - Returns: a SKSpriteNode that represents a vehicle wheel and can be fither used as a component
+    func produce(using builder: WheelBuilder) -> SKSpriteNode {
+        fatalError(#function + " has not been implemented")
+        return SKSpriteNode(color: .clear, size: .zero)
+    }
 }
