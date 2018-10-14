affa8484ecc8575a88e2b4d8e2ab0b2479f34a36
Astemir Eleev <astemireleev@gmail.com>
2018-10-14 15:34:27 +0300

    Refactored builders

--- ios-spritekit-physics-racing Shared/Components/WheelBuilder.swift
+++ ios-spritekit-physics-racing Shared/Components/WheelBuilder.swift
@@ -8,10 +8,21 @@
 
 import SpriteKit
 
+protocol WheelSuspensionBuilder {
+    var dapming: CGFloat { get }
+    var frequiency: CGFloat { get }
+    var shockPostColor: SKColor { get }
+    var shockPostSize: CGSize { get }
+    var shockPostOffset: CGPoint { get }
+    var shockPostPosition: CGPoint { get }
+}
+
 protocol WheelBuilder {
     var texture: SKTexture { get }
     var size: CGSize { get }
     var zPostiion: CGFloat { get }
     var position: CGPoint { get }
     var allowsRotation: Bool { get }
+    var offset: CGPoint { get }
+    
 }
