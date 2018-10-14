71d91a85a1d60e8074447e3c292a943ee5788a0b
Astemir Eleev <astemireleev@gmail.com>
2018-10-14 15:34:41 +0300

    Created VehicleType enum

---
+++ ios-spritekit-physics-racing Shared/Components/VehicleType.swift
@@ -0,0 +1,15 @@
+//
+//  VehicleType.swift
+//  ios-spritekit-physics-racing
+//
+//  Created by Astemir Eleev on 14/10/2018.
+//  Copyright © 2018 Astemir Eleev. All rights reserved.
+//
+
+import Foundation
+
+enum VehicleType {
+    case jeep
+    case humvee
+}
+
