acdfa9e4793a0cf76cead859419a65b4d2cb4a02
Astemir Eleev <astemireleev@gmail.com>
2018-10-15 11:18:29 +0300

    Implemented WheelNode component

---
+++ ios-spritekit-physics-racing Shared/Components/WheelNode.swift
@@ -0,0 +1,25 @@
+//
+//  WheelNode.swift
+//  ios-spritekit-physics-racing
+//
+//  Created by Astemir Eleev on 15/10/2018.
+//  Copyright © 2018 Astemir Eleev. All rights reserved.
+//
+
+import SpriteKit
+
+class WheelNode: SKSpriteNode {
+    
+    init(builder: WheelBuilder) {
+        super.init(texture: builder.texture, color: .clear, size: builder.size)
+        
+        zPosition = builder.zPostiion
+        position = builder.position
+        physicsBody = SKPhysicsBody(texture: builder.texture, alphaThreshold: 0.0, size: self.size)
+        physicsBody?.allowsRotation = builder.allowsRotation
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
