bf227bd6d627e02e6ed043b0a600741014846e82
Astemir Eleev <astemireleev@gmail.com>
2018-10-15 11:12:55 +0300

    Implemented JeepWheelSuspensionBuilder struct

---
+++ ios-spritekit-physics-racing Shared/Components/Builders/Jeep/JeepWheelSuspensionBuilder.swift
@@ -0,0 +1,33 @@
+//
+//  JeepWheelSuspensionBuilder.swift
+//  ios-spritekit-physics-racing
+//
+//  Created by Astemir Eleev on 15/10/2018.
+//  Copyright © 2018 Astemir Eleev. All rights reserved.
+//
+
+import SpriteKit
+
+struct JeepWheelSuspensionBuilder: WheelSuspensionBuilder {
+
+    // MARK: - Properties
+    
+    var dapming: CGFloat                = 0.75
+    var frequiency: CGFloat             = 5
+    var wheelOffset: CGPoint            = CGPoint(x: 0, y: 65)
+    var shockPostColor: SKColor         = .blue
+    var shockPostSize: CGSize           = CGSize(width: 7, height: 65)
+    var slideLoweLimit: CGFloat         = 5
+    
+    var shockPostOffset: CGPoint
+    var shockPostPosition: CGPoint
+    var springAttachmentPoint: CGPoint
+    
+    // MRAK: - Initializers
+    
+    init(shockPostOffset: CGPoint, shockPostPosition: CGPoint, springAttachmentPoint: CGPoint) {
+        self.shockPostOffset = shockPostOffset
+        self.shockPostPosition = shockPostPosition
+        self.springAttachmentPoint = springAttachmentPoint
+    }
+}
