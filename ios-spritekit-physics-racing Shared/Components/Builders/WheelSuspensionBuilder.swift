76e2d0dc09ea48ef0188bdd159fb89123a4fbfd3
Astemir Eleev <astemireleev@gmail.com>
2018-10-15 11:13:35 +0300

    Implemented WheelSuspensionBuilder protocol

---
+++ ios-spritekit-physics-racing Shared/Components/Builders/WheelSuspensionBuilder.swift
@@ -0,0 +1,21 @@
+//
+//  WheelSuspensionBuilder.swift
+//  ios-spritekit-physics-racing iOS
+//
+//  Created by Astemir Eleev on 15/10/2018.
+//  Copyright © 2018 Astemir Eleev. All rights reserved.
+//
+
+import SpriteKit
+
+protocol WheelSuspensionBuilder {
+    var dapming: CGFloat { get }
+    var frequiency: CGFloat { get }
+    var wheelOffset: CGPoint { get }
+    var shockPostColor: SKColor { get }
+    var shockPostSize: CGSize { get }
+    var shockPostOffset: CGPoint { get }
+    var shockPostPosition: CGPoint { get }
+    var slideLoweLimit: CGFloat { get }
+    var springAttachmentPoint: CGPoint { get }
+}
