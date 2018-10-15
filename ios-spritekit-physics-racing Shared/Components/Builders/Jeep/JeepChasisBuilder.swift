6f8d41e13a450a00709b12e02ff1afce36a93a4b
Astemir Eleev <astemireleev@gmail.com>
2018-10-15 11:13:21 +0300

    Implemented JeepChasisBuilder struct

---
+++ ios-spritekit-physics-racing Shared/Components/Builders/Jeep/JeepChasisBuilder.swift
@@ -0,0 +1,39 @@
+//
+//  JeepChasisBuilder.swift
+//  ios-spritekit-physics-racing
+//
+//  Created by Astemir Eleev on 15/10/2018.
+//  Copyright © 2018 Astemir Eleev. All rights reserved.
+//
+
+import SpriteKit
+
+struct JeepChasisBuilder: ChasisBuilder {
+    
+    // MARK: - Properties
+    
+    var texture: SKTexture {
+        let assetName = "car-body"
+        var texture: SKTexture!
+        
+        #if os(macOS)
+        texture = SKTexture(image: NSImage(named: assetName)!)
+        #endif
+        
+        #if os(iOS)
+        texture = SKTexture(image: UIImage(named: assetName)!)
+        #endif
+        
+        return texture
+    }
+    var position: CGPoint
+    
+    var size: CGSize        = CGSize(width: 400, height: 112.3)
+    var zPosition: CGFloat  = 8
+    
+    // MARK: - Initializers
+    
+    init(position: CGPoint) {
+        self.position = position
+    }
+}
