63fcf71a52abf97e782f26474438590da206acdb
Astemir Eleev <astemireleev@gmail.com>
2018-10-17 16:33:08 +0300

    Updated

---
+++ ios-spritekit-physics-racing Shared/Components/Builders/Humvee/HumveeChasisBuilder.temp_caseinsensitive_rename.swift
@@ -0,0 +1,9 @@
+//
+//  HumveeChasisBuilder.swift
+//  ios-spritekit-physics-racing
+//
+//  Created by Astemir Eleev on 17/10/2018.
+//  Copyright © 2018 Astemir Eleev. All rights reserved.
+//
+
+import Foundation
