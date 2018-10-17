7b27c3c614d85f48d61b58f963d7b9c20dfa1656
Astemir Eleev <astemireleev@gmail.com>
2018-10-17 14:03:20 +0300

    Implemented JeepWheelsLocationBuilder struct

---
+++ ios-spritekit-physics-racing Shared/Components/Builders/Jeep/JeepWheelsLocationBuilder.swift
@@ -0,0 +1,42 @@
+//
+//  JeepWheelsLocationBuilder.swift
+//  ios-spritekit-physics-racing
+//
+//  Created by Astemir Eleev on 17/10/2018.
+//  Copyright © 2018 Astemir Eleev. All rights reserved.
+//
+
+import Foundation
+
+struct JeepWheelsLocationBuilder: WheelsLocationBuilder {
+    
+    // MARK: - Properties
+    
+    var locations: [WheelLocation]
+    
+    // MARK: - Initializers
+    
+    init(chasis: ChasisNode) {
+        locations = []
+        locations.reserveCapacity(2)
+        
+        let chasisHalfX = chasis.size.width / 2
+        let chasisLeftHalfX = chasis.position.x - chasisHalfX
+        let chasisRightHalfX = chasis.position.x + chasisHalfX
+        
+        let leftWheelPosition = CGPoint(x: chasisLeftHalfX + 68, y: chasis.position.y - 65)
+        let rightWheelPosition = CGPoint(x: chasisRightHalfX - 72, y: chasis.position.y - 65)
+        
+        let chasisShockY = chasis.position.y - (65 / 2)
+        
+        
+        let leftWheel = WheelLocation.init(wheelLocation: leftWheelPosition,
+                                           shockPostLocation: CGPoint(x: leftWheelPosition.x, y: chasisShockY),
+                                           springAttachmentLocation: CGPoint(x: leftWheelPosition.x, y: chasis.position.y))
+        
+        let rightWheel = WheelLocation.init(wheelLocation: rightWheelPosition,
+                                            shockPostLocation: CGPoint(x: rightWheelPosition.x, y: chasisShockY),
+                                            springAttachmentLocation: CGPoint(x: rightWheelPosition.x, y: chasis.position.y))
+        locations += [leftWheel, rightWheel]
+    }
+}
