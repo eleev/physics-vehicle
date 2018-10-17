1cad2001fc63b6bce2255436a09b873cf77727f4
Astemir Eleev <astemireleev@gmail.com>
2018-10-17 14:03:03 +0300

    Implemented WheelLocation struct

---
+++ ios-spritekit-physics-racing Shared/Components/Builders/WheelLocation.swift
@@ -0,0 +1,15 @@
+//
+//  WheelLocation.swift
+//  ios-spritekit-physics-racing
+//
+//  Created by Astemir Eleev on 17/10/2018.
+//  Copyright © 2018 Astemir Eleev. All rights reserved.
+//
+
+import Foundation
+
+struct WheelLocation {
+    private(set) var wheelLocation: CGPoint
+    private(set) var shockPostLocation: CGPoint
+    private(set) var springAttachmentLocation: CGPoint
+}
