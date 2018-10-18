a9e053a68be9186e3865dccdbca683b7654bd962
Astemir Eleev <astemireleev@gmail.com>
2018-10-18 19:05:53 +0300

    Implemented HumveeEngineConfiguration struct

---
+++ ios-spritekit-physics-racing Shared/Components/Builders/Humvee/HumveeEngineConfiguration.swift
@@ -0,0 +1,15 @@
+//
+//  HumveeEngineConfiguration.swift
+//  ios-spritekit-physics-racing
+//
+//  Created by Astemir Eleev on 18/10/2018.
+//  Copyright © 2018 Astemir Eleev. All rights reserved.
+//
+
+import CoreGraphics
+
+struct HumveeEngineConfiguration: EngineConfiguration {
+    var horsepower: Int = 70
+    var forwardSpeed: CGFloat = 780
+    var backwardSpeed: CGFloat = -550
+}
