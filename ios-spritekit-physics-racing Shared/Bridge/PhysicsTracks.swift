e3d27d15ee1e83740621daf4b13847f5dc71df7e
Astemir Eleev <astemireleev@gmail.com>
2018-10-20 19:29:13 +0300

    Created PhysicsTracks class

---
+++ ios-spritekit-physics-racing Shared/Bridge/PhysicsTracks.swift
@@ -0,0 +1,86 @@
+//
+//  PhysicsTracks.swift
+//  ios-spritekit-physics-racing
+//
+//  Created by Astemir Eleev on 19/10/2018.
+//  Copyright © 2018 Astemir Eleev. All rights reserved.
+//
+
+import SpriteKit
+
+#warning("This class should not be used until it's properly implemented")
+class PhysicsTracks: SKNode {
+    
+    private var tracks = [ChainNode]()
+    
+    override init() {
+        super.init()
+        
+//        let halfOrange = SKColor.init(red: 1.0, green: 1.0, blue: 0.0, alpha: 0.5)
+//        let size = CGSize(width: 50, height: 5.0)
+//        
+//        let elements = 30
+//        
+//        for index in 0...elements {
+//            
+//        }
+        
+//        let node01 = ChainNode(color: halfOrange, size: size)
+//        node01.position = CGPoint(x: 0.0, y: 0.0)
+//        addChild(node01)
+//        tracks += [node01]
+//
+//        let node02 = ChainNode(color: halfOrange, size: size)
+//        node02.position = CGPoint(x: size.width * 1, y: 50.0)
+//        addChild(node02)
+//        tracks += [node02]
+//
+//        let node03 = ChainNode(color: halfOrange, size: size)
+//        node03.position = CGPoint(x: size.width * 2, y: 50.0)
+//        addChild(node03)
+//        tracks += [node03]
+//
+//        let node04 = ChainNode(color: halfOrange, size: size)
+//        node04.position = CGPoint(x: size.width * 3, y: 50.0)
+//        addChild(node04)
+//        tracks += [node04]
+//
+//        let node05 = ChainNode(color: halfOrange, size: size)
+//        node05.position = CGPoint(x: size.width * 4, y: 50.0)
+//        addChild(node05)
+//        tracks += [node05]
+//
+//
+//
+//        let bottomNode01 = node01.copy() as? ChainNode
+//        bottomNode01?.position.y = -50
+//        addChild(bottomNode01!)
+//        tracks += [bottomNode01!]
+//
+//        let bottomNode02 = node02.copy() as? ChainNode
+//        bottomNode02?.position.y = -50
+//        addChild(bottomNode02!)
+//        tracks += [bottomNode02!]
+//
+//        let bottomNode03 = node03.copy() as? ChainNode
+//        bottomNode03?.position.y = -50
+//        addChild(bottomNode03!)
+//        tracks += [bottomNode03!]
+//
+//        let bottomNode04 = node04.copy() as? ChainNode
+//        bottomNode04?.position.y = -50
+//        addChild(bottomNode04!)
+//        tracks += [bottomNode04!]
+//
+//        let bottomNode05 = node05.copy() as? ChainNode
+//        bottomNode05?.position.y = -50
+//        addChild(bottomNode05!)
+//        tracks += [bottomNode05!]
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
